c4c86896e9e4b808a21226957a7c0ef37bbc9d39
zikra Faraz <zikrafaraz736@gmail.com>
2026-01-18 19:31:09 +0530

    ccode

--- src/dp/fibonacci.java
+++ src/dp/fibonacci.java
@@ -22,5 +22,18 @@ public class fibonacci {
         }
         return dp[n];
     }
+ public static int space(int n ){
+        if(n<=1) return n;
+        int prev2 = 0;
+        int prev = 1;
+        int curr = 0;
+        for(int i = 2 ; i<= n ; i++){
+            curr = prev + prev2;
+            prev2  = prev;
+            prev = curr;
+        }
 
+
+        return prev;
+ }
 }
