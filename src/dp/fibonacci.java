3c90176826872670b7c2b32f7d5f07920e63c2e4
zikra Faraz <zikrafaraz736@gmail.com>
2026-01-18 19:25:29 +0530

    add fibo code

---
+++ src/dp/fibonacci.java
@@ -0,0 +1,26 @@
+package dp;
+
+public class fibonacci {
+    public static int recur(int n){
+        if(n <= 1) return n;
+        return recur(n-1)+recur(n-2);
+    }
+    public static int memoiz(int n , int[] dp){
+      if(n<=1) return n;
+      if(dp[n] != -1) return dp[n];
+      dp[n] =  memoiz(n-1 , dp) + memoiz( n-2 , dp);
+      return dp[n];
+    }
+    public static int tabul(int n ){
+       if(n<=1) return n;
+       int[] dp = new int[n+1];
+       dp[0] = 0;
+       dp[1] =1;
+
+        for (int i = 2; i <= n ; i++) {
+            dp[i]  = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+
+}
--- src/hello.java
+++
@@ -1,8 +0,0 @@
-public class hello {
-    public  static void  print(){
-        System.out.println("nothing");
-    }
-    public static void main(String[] args) {
-
-    }
-}
